b17259db8dfd108b60864ac4a8fd5578830978dc
Soham Pradhan <117765145+s-o-ham@users.noreply.github.com>
2024-10-11 19:43:57 +0530

    Last Digit.java

--- Practical/Last Digit.java
+++ Practical/Last Digit.java
@@ -4,6 +4,7 @@ class HelloWorld {
         Scanner sc = new Scanner(System.in);
        System.out.println("Enter a number");
        int n = sc.nextInt();
-       System.out.println(n%10);
+       int y = Math.abs(n);
+       System.out.println(y%10);
     }
 }
